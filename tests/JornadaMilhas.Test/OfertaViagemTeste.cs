af1c15dc0e387bd4fe6322c7883bc1f2b8821f47
MateusMo <mat.marq.oliveira@gmail.com>
2024-06-26 08:13:08 -0300

    Implementando mais testes

--- tests/JornadaMilhas.Test/OfertaViagemTeste.cs
+++ tests/JornadaMilhas.Test/OfertaViagemTeste.cs
@@ -33,5 +33,21 @@ namespace JornadaMilhas.Test
             Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
             Assert.False(oferta.EhValido); 
         }
+
+        [Fact]
+        public void TestandoPrecoNegativo()
+        {
+            //cenário - arrange
+            Rota rota = new("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new(DateTime.Now.AddDays(-20), DateTime.Now.AddDays(2));
+            double preco = -100.0;
+
+            //ação - act
+            OfertaViagem oferta = new(rota, periodo, preco);
+
+            //validação - assert
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
     }
 }
