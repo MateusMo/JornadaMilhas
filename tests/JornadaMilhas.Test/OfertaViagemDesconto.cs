35c53974a04d4299926fb4d064aa2cee96fbc49c
MateusMo <mat.marq.oliveira@gmail.com>
2024-06-28 08:34:37 -0300

    Adicionando desconto teste e implementação

---
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,26 @@
+﻿using JornadaMilhasV1.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualizadoQuandoAplicadoDesconto()
+        {
+            Rota rota = new Rota("OrigemA","DestinoB");
+            Periodo periodo = new(DateTime.Now, DateTime.Now.AddDays(5));
+            double precoOriginal = 100.00;
+            double desconto = 20.00;
+            double precoComDesconto = precoOriginal - desconto;
+            OfertaViagem oferta = new(rota, periodo, precoOriginal);
+
+            oferta.Desconto = desconto;
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+    }
+}
