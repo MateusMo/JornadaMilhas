f85f113999bcd9f54d8569ed6d0466ac2cbedd7d
MateusMo <mat.marq.oliveira@gmail.com>
2024-07-02 08:03:26 -0300

    Adicionando testes de contagem de erro

--- src/JornadaMilhasV1/Modelos/OfertaViagem.cs
+++ src/JornadaMilhasV1/Modelos/OfertaViagem.cs
@@ -9,6 +9,7 @@ namespace JornadaMilhasV1.Modelos;
 
 public class OfertaViagem : Valida
 {
+    public const double DESCONTO_MAXIMO = 0.7;
     private double desconto;
 
     public int Id { get; set; }
@@ -21,7 +22,15 @@ public class OfertaViagem : Valida
         set
         {
             desconto = value;
-            Preco -= desconto;
+            
+            if(desconto >= Preco)
+            {
+                Preco *= (1 - DESCONTO_MAXIMO);
+            }
+            else
+            {
+                Preco -= desconto;
+            }
         }
     }
 
@@ -45,11 +54,13 @@ public class OfertaViagem : Valida
         {
             Erros.RegistrarErro(Periodo.Erros.Sumario);
         }
-        else if (Rota == null || Periodo == null)
+
+        if (Rota == null || Periodo == null)
         {
             Erros.RegistrarErro("A oferta de viagem não possui rota ou período válidos.");
         }
-        else if (Preco <= 0)
+
+        if (Preco <= 0)
         {
             Erros.RegistrarErro("O preço da oferta de viagem deve ser maior que zero.");
         }
--- tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
+++ tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -22,5 +22,33 @@ namespace JornadaMilhas.Test
             oferta.Desconto = desconto;
             Assert.Equal(precoComDesconto, oferta.Preco);
         }
+
+        [Fact]
+        public void RetornaDescontoMaximoQuandoValorDescontoMaiorQuePreco()
+        {
+            Rota rota = new Rota("OrigemA", "DestinoB");
+            Periodo periodo = new(DateTime.Now, DateTime.Now.AddDays(5));
+            double precoOriginal = 100.00;
+            double desconto = 120.00;
+            double precoComDesconto = 30.00;
+            OfertaViagem oferta = new(rota, periodo, precoOriginal);
+
+            oferta.Desconto = desconto;
+            Assert.Equal(precoComDesconto, oferta.Preco,0.001);
+        }
+
+        [Fact]
+        public void RetornaTresErrosDeValidacaoQuandoRotaPeriodoEPrecoSaoInvalidos()
+        {
+            //arrange
+            Rota rota = null;
+            Periodo periodo = new(new DateTime(2024,6,1),new DateTime(2024,5,10));
+            double preco = -100;
+            int quantidadeEsperada = 3;
+            //act
+            OfertaViagem oferta = new(rota, periodo, preco);
+            //asset
+            Assert.Equal(quantidadeEsperada, oferta.Erros.Count());
+        }
     }
 }
